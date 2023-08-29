19048bc266084e1925c55e41e55eb622cb73f3fd
PixelJaeger <95146121+PixelJaeger@users.noreply.github.com>
2023-08-29 19:55:56 +0200

    Update nuka_led.py
    added random length pulse (start, stop and step are semi-random)

--- nuka_led.py
+++ nuka_led.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from gpiozero import PWMLED
 from time import sleep
-import argparse, numpy
+import argparse, numpy, random
 
 led_mid = PWMLED(24)
 led_up = PWMLED(22)
@@ -10,7 +10,6 @@ led_right = PWMLED(23)
 led_left = PWMLED(16)
 
 parser = argparse.ArgumentParser()
-
 parser.add_argument('-b', '--blink', action='store_true', help="blinking led")
 parser.add_argument('-o', '--opposite', action='store_true', help="polarcoords animation")
 parser.add_argument('-c', '--circle', action='store_true', help="circle animation")
@@ -19,11 +18,36 @@ parser.add_argument('-a', '--all', action='store_true', help="full animation-cyc
 parser.add_argument('-p', '--pulse', action='store_true', help="pulse animation")
 parser.add_argument('integers', metavar='N', type=float, nargs='+', help='an value for the args.integers')
 parser.add_argument('-t', '--test', action='store_true', help="test")
-
+parser.add_argument('-r', '--rndpulse', action='store_true', help="random value pulses")
 args = parser.parse_args()
-
 delay = float(args.integers[0])
 mid_led = 0
+led_pulse_step = ['0.025','0.05','0.075','0.1']
+led_pulse_start = ['0.1','0.2','0.3','0.4']
+led_pulse_stop = ['1','0.9','0.8','0.7']
+rnd = 0
+
+def random_pulse():
+    global rnd, pulse_step, pulse_start, pulse_stop
+    rnd = random.randrange(3,10,1)
+    pulse_step = float(random.choice(led_pulse_step))
+    pulse_start = float(random.choice(led_pulse_start))
+    pulse_stop = float(random.choice(led_pulse_stop))
+    for a in range(rnd):
+        for x in numpy.arange(pulse_start,pulse_stop,pulse_step):
+            led_up.value = x
+            led_mid.value = x
+            led_down.value = x
+            led_left.value = x
+            led_right.value = x
+            sleep(0.05)
+        for x in reversed(numpy.arange(pulse_start,pulse_stop,pulse_step)):
+            led_up.value = x
+            led_mid.value = x
+            led_down.value = x
+            led_left.value = x
+            led_right.value = x
+            sleep(0.05)
 
 def cycle_polar():
     global mid_led
@@ -121,6 +145,11 @@ if args.full:
 if args.pulse:
     while True:
         cycle_pulse()
+
+if args.rndpulse:
+    while True:
+        random_pulse()
+
 
 if args.all:
     while True:
